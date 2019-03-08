a2b821e5d1f2ff6f1e82ff7af34ff50da012273b
TarasekM <rezkth@gmail.com>
2019-03-08 09:10:48 +0100

    Rename MainActivity and all related files to LoginActivity.

---
+++ app/src/main/java/com/apki/e_tests/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.apki.e_tests;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        configureLoginButton();
+
+    }
+
+    private void configureLoginButton(){
+        Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
