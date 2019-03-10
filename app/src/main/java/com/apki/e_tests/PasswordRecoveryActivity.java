4a64b6b32f4592a2d287c87de2c4a6e0d7e9d0be
TarasekM <rezkth@gmail.com>
2019-03-10 19:25:58 +0100

    Add password recovery activity.

---
+++ app/src/main/java/com/apki/e_tests/PasswordRecoveryActivity.java
@@ -0,0 +1,17 @@
+package com.apki.e_tests;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class PasswordRecoveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_recovery);
+
+    }
+}
