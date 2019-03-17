bc22cf3db0deac43f996d1df80fbc7e0796830c5
mateuszlatkowski <mateuszlatkowski@smcebi.edu.pl>
2019-03-17 13:00:08 +0100

    Check if the fields aren't empty.

--- app/src/main/java/com/apki/e_tests/PasswordRecoveryActivity.java
+++ app/src/main/java/com/apki/e_tests/PasswordRecoveryActivity.java
@@ -5,17 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PasswordRecoveryActivity extends AppCompatActivity {
+
+    EditText input;
+    Button reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_password_recovery);
+
+        input = (EditText)findViewById(R.id.inputEmail);
+        reset = (Button)findViewById(R.id.login2);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (input.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Puste pola!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
     }
 
     @Override
