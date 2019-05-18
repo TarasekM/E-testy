e6e64265c2e185abcd7a9edf6ee1bc370422f969
TarasekM <rezkth@gmail.com>
2019-05-18 21:03:37 +0200

    Add Class model

---
+++ app/src/main/java/com/apki/e_tests/Class.java
@@ -0,0 +1,10 @@
+package com.apki.e_tests;
+
+import java.util.ArrayList;
+
+public class Class {
+    private ArrayList<User> students;
+    private ArrayList<User> teachers;
+    private ArrayList<Test> tests;
+    private User owner;
+}
